d60b0deb862431b2f70ce2629efe3b324c9d7886
John Clarke <john@abbeywebs.com.au>
2021-05-14 15:00:33 +1000

    Completed Frontend and set up Postman JWTs

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'johnatborpa', // the auth0 domain prefix
+    url: 'johnatborpa.au', // the auth0 domain prefix
     audience: 'coffeeshop', // the audience set for the auth0 app
     clientId: 'DBXy7kNkOG1YKrHCcSu1QY4Euj534MoS', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
